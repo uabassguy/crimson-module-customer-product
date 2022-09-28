6f2ce8a4d587a9c37247c2229171969cc3438825
Dave Wilson <dwilson@riversagency.com>
2022-09-28 09:10:02 -0700

    attempt to fix non working controller

--- Controller/Customer/Account/ProductRange/IndexController.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Crimson\ProductRange\Controller\Customer\Account\ProductRange;
-
-class Index extends \Magento\Framework\App\Action\Action {
-    public function execute() {
-        $this->_view->loadLayout();
-        $this->_view->renderLayout();
-    }
-}
---
+++ Controller/Customer/Account/ProductRangeController.php
@@ -0,0 +1,31 @@
+<?php
+namespace Crimson\ProductRange\Controller\Customer\Account;
+
+use Magento\Framework\App\Action\HttpGetActionInterface as HttpGetActionInterface;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class ProductRangeController extends \Magento\Customer\Controller\AbstractAccount implements HttpGetActionInterface
+{
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+
+    public function execute() {
+        $this->_view->loadLayout();
+        $this->_view->renderLayout();
+    }
+}
