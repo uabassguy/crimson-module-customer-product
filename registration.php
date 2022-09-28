62dd73859c12927c927083231bbb4e02a822a62b
Dave Wilson <dwilson@riversagency.com>
2022-09-28 08:50:17 -0700

    add module xml

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Crimson_CustomerProduct',
+    'Crimson_ProductRange',
     __DIR__
 );
