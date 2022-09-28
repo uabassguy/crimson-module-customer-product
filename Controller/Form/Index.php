171c05ebd22c61d8162da9f54a7853eec8efab9d
Dave Wilson <dwilson@riversagency.com>
2022-09-28 10:26:17 -0700

    asdasdasda

--- Controller/Form/Index.php
+++ Controller/Form/Index.php
@@ -1,12 +1,10 @@
 <?php
-namespace Crimson\ProductRange\Controller;
-die();
+namespace Crimson\ProductRange\Controller\Form;
+
 use Magento\Framework\App\Action\HttpGetActionInterface as HttpGetActionInterface;
-use Magento\Sales\Controller\OrderInterface;
-use Magento\Framework\App\Action\Context;
 use Magento\Framework\View\Result\PageFactory;
 
-class Index extends \Magento\Framework\App\Action\Action implements OrderInterface, HttpGetActionInterface
+class Index implements HttpGetActionInterface
 {
     /**
      * @var PageFactory
@@ -14,19 +12,17 @@ class Index extends \Magento\Framework\App\Action\Action implements OrderInterfa
     protected $resultPageFactory;
 
     /**
-     * @param Context $context
      * @param PageFactory $resultPageFactory
      */
     public function __construct(
-        Context $context,
         PageFactory $resultPageFactory
     ) {
         $this->resultPageFactory = $resultPageFactory;
-        parent::__construct($context);
     }
 
     public function execute() {
-        $this->_view->loadLayout();
-        $this->_view->renderLayout();
+        die('ok');
+        //$this->_view->loadLayout();
+        //$this->_view->renderLayout();
     }
 }
