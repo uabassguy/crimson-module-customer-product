0bfa651c003db3574659f7850435a0384cf64fb4
Dave Wilson <dwilson@riversagency.com>
2022-09-28 09:59:04 -0700

    asdasdasda

--- Controller/Index.php
+++ Controller/Index.php
@@ -1,12 +1,28 @@
 <?php
 namespace Crimson\ProductRange\Controller;
 
-class Index extends \Magento\Framework\App\Action\Action
+use Magento\Framework\App\Action\HttpGetActionInterface as HttpGetActionInterface;
+use Magento\Sales\Controller\OrderInterface;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends \Magento\Framework\App\Action\Action implements OrderInterface, HttpGetActionInterface
 {
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
     public function __construct(
-        \Magento\Framework\App\Action\Context $context)
-    {
-        return parent::__construct($context);
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
     }
 
     public function execute() {
