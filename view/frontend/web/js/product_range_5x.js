6a74fe74ed8f61890fe5aa7b7828958b1ad08238
Dave Wilson <dwilson@riversagency.com>
2022-09-29 15:23:24 -0700

    debug

---
+++ view/frontend/web/js/product_range_5x.js
@@ -0,0 +1,17 @@
+define([
+    'mage/translate',
+    'jquery'
+], function($t) {
+    'use strict';
+
+    return function(rules) {
+        rules['high-multiple-5x'] = {
+            handler: function (value) {
+                let lowVal = jQuery('input.low').value();
+                return (value <= lowVal * 5) && (value > lowVal);
+            },
+            message: $t('Value must be greater than Low and Less than (Low x 5)')
+        };
+        return rules;
+    };
+});
