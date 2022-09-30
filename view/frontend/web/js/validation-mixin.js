d0202234793a105250bd7c03115bd0ff7b451cd9
Dave Wilson <dwilson@riversagency.com>
2022-09-29 18:07:44 -0700

    change val function

--- view/frontend/web/js/validation-mixin.js
+++ view/frontend/web/js/validation-mixin.js
@@ -5,7 +5,7 @@ define(['jquery'], function($) {
         $.validator.addMethod(
             'high-multiple-5x',
             function(value, element) {
-                let lowVal = $('input.low').value();
+                let lowVal = $('input.low').val();
                 return (value >= lowVal * 5) && (value > lowVal);
             },
             $.mage.__('Value must be greater than Low and Less than (Low x 5)')
